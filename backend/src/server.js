f708cb77228daa77b9c1f5aa81fabe856ea9368e
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2025-11-01 18:51:35 +0200

    inital the app

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
