a2df5a95f27970db40b5cd60233b8006fc16bea2
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2025-11-01 19:16:23 +0200

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,23 @@
 import express from "express";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 import "dotenv/config";
 
 const app = express();
+
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
+
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("/", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
-import React from "react";
-
 const App = () => {
-  return <div>test</div>;
+  return <div>frontend</div>;
 };
 
 export default App;
