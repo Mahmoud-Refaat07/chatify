df60a56a6a094da591cf6af390c724d1f2347619
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2025-11-02 07:41:46 +0200

    backend refactoring

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,8 +1,10 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 
+const { JWT_SECRET_KEY, NODE_ENV } = process.env;
+
 export const generateToken = (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+  const token = jwt.sign({ userId }, JWT_SECRET_KEY, {
     expiresIn: "7d",
   });
 
@@ -12,6 +14,6 @@ export const generateToken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true, // prevent XSS attack: cross-site scripting
     sameSite: "strict", // prevent CSRF attack
-    secure: process.env.NODE_ENV === "development" ? false : true, //HTTPS at production , HTTP at development
+    secure: NODE_ENV === "development" ? false : true, //HTTPS at production , HTTP at development
   });
 };
