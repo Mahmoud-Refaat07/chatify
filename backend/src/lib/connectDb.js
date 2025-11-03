83204e5e4856fc9a3c0508fcd0220db415781ed1
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2025-11-03 05:06:50 +0200

    refactor

--- backend/src/lib/connectDb.js
+++ backend/src/lib/connectDb.js
@@ -6,8 +6,8 @@ export const connectDb = async () => {
   try {
     await mongoose.connect(URI);
     console.log("connected to database");
-  } catch {
-    console.log("failed to connect to database");
+  } catch (error) {
+    console.log("failed to connect to database", error);
     process.exit(1);
   }
 };
