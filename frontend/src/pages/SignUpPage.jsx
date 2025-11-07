3db4aeba7651b2139620b4c33190d6b6166d5dc6
MahmoudRefaat07 <mahmoud.refaat.career@gmail.com>
2025-11-07 20:40:27 +0200

    signup implemented in frontend

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,146 @@
+import { useState } from "react";
+import BorderAnimatedContainer from "../components/BorderAnimatedContainer.jsx";
+import { useAuthStore } from "../store/useAuthStore.js";
+import { MessageCircleIcon, UserIcon } from "lucide-react";
+import { LoaderIcon } from "react-hot-toast";
+import { Link } from "react-router";
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const { isSigningUp, signup } = useAuthStore();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signup(formData);
+  };
+
+  return (
+    <div className="w-full flex items-center justify-center p-4 bg-slate-900">
+      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]">
+        <BorderAnimatedContainer>
+          <div className="w-full flex flex-col md:flex-row">
+            {/* FORM CLOUMN - LEFT SIDE */}
+            <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30">
+              <div className="w-full max-w-md">
+                {/* HEADING TEXT */}
+                <div className="text-center mb-8">
+                  <MessageCircleIcon className="w-12 h-12 mx-auto text-slate-400 mb-4" />
+                  <h2 className="text-2xl font-bold text-slate-200 mb-2">
+                    Create Account
+                  </h2>
+                  <p className="text-slate-400">Sign up for a new account</p>
+                </div>
+
+                {/* FORM */}
+                <form onSubmit={handleSubmit} className="space-y-4">
+                  {/* USERNAME INPUT */}
+                  <div>
+                    <label className="auth-input=label">Username</label>
+                    <div className="relative">
+                      <UserIcon className="auth-input-icon" />
+                      <input
+                        type="text"
+                        className="input"
+                        placeholder="JohnDoe"
+                        value={formData.username}
+                        onChange={(e) => {
+                          setFormData({
+                            ...formData,
+                            username: e.target.value,
+                          });
+                        }}
+                      />
+                    </div>
+                  </div>{" "}
+                  {/* EMAIL INPUT */}
+                  <div>
+                    <label className="auth-input=label">Email</label>
+                    <div className="relative">
+                      <UserIcon className="auth-input-icon" />
+                      <input
+                        type="email"
+                        className="input"
+                        placeholder="hohndoe@mail.com"
+                        value={formData.email}
+                        onChange={(e) => {
+                          setFormData({
+                            ...formData,
+                            email: e.target.value,
+                          });
+                        }}
+                      />
+                    </div>
+                  </div>{" "}
+                  {/* PASSWORD INPUT */}
+                  <div>
+                    <label className="auth-input=label">Password</label>
+                    <div className="relative">
+                      <UserIcon className="auth-input-icon" />
+                      <input
+                        type="password"
+                        className="input"
+                        placeholder="*********"
+                        value={formData.password}
+                        onChange={(e) => {
+                          setFormData({
+                            ...formData,
+                            password: e.target.value,
+                          });
+                        }}
+                      />
+                    </div>
+                  </div>
+                  {/* SUBMIT BUTTON */}
+                  <button
+                    className="auth-btn"
+                    type="submit"
+                    disabled={isSigningUp}
+                  >
+                    {isSigningUp ? (
+                      <LoaderIcon className="w-full h-5 animate-spin text-center" />
+                    ) : (
+                      "Create Account"
+                    )}
+                  </button>
+                </form>
+                <div className="mt-6 text-center">
+                  <Link to={"/login"} className="auth-link">
+                    Already have account? Login
+                  </Link>
+                </div>
+              </div>
+            </div>
+            {/* FORM ILLUSTRATION - RIGHT SIDE*/}
+            <div className="hidden md:w-1/2 md:flex items-center justify-center p-6 bg-gradient-to-bl from-slate-800/20 to-transparent">
+              <div>
+                <img
+                  src="/signup.png"
+                  alt="People using mobile devices"
+                  className="w-full h-auto object-contain"
+                />
+                <div className="mt-6 text-center">
+                  <h3 className="text-xl font-medium text-cyan-400">
+                    Start Your Journey Today
+                  </h3>
+
+                  <div className="mt-4 flex justify-center gap-4">
+                    <span className="auth-badge">Free</span>
+                    <span className="auth-badge">Easy Setup</span>
+                    <span className="auth-badge">Private</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </BorderAnimatedContainer>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
